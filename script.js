cf22763d91a6778594ab53aef7c052c9eee44ac6
MrFluffyBunny <do_you_rememberme@hotmail.co.uk>
2024-05-06 21:53:29 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const quotes = [
+    "For death and glory.",
+    "How did it come to this?",
+    "Ride to ruin and the world’s ending!",
+    "Take up my seat in the Golden Hall.",
+    "No parent should have to bury their child.",
+    "Look at my men. Their courage hangs by a thread.",
+    "Let this be the hour when we draw swords together.",
+    "So much death. What can men do against such reckless hate?",
+    "Now for wrath, now for ruin, and the red dawn. Foreth, Eorlingas!",
+    "If this is to be pour end, then I would have them make such an end, as to be worthy of remembrance.",
+    "Ride now!",
+    "We shall have peace.",
+    "Arise, arise, Riders of!",
+    "A sword day, a red day, ere the sun rises!",
+    "Spears shall be shaken, shields shall be splintered.",
+    "The days have gone down in the West behind the hills into shadow.",
+    "Crops can be resewn, homes rebuilt. Within these walls, we will outlast them.",
+    "What would you have me do?",
+    "Hammerhand shall sound in the deep one last time.",
+    "They will break upon this fortress like water upon rock.",
+    "You shall live to see these days renewed. No more despair.",
+    "I would have you smile again, not grieve for those whose time has come.",
+    "I go to my fathers in whose mighty company I shall not now feel ashamed.",
+    "Where is the horn that was blowing? They have passed like rain on the mountain, like wind in the meadow.",
+    "We shall have peace, when the lives of the soldiers whose bodies were hewn even as they died against the gates of the Hornberg, are avenged!"
+];
+
+const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+console.log(randomQuote);
